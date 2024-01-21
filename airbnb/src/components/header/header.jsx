608b19e85d4331d2f49dae5bb39d6b1d165ec1b5
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-21 14:10:35 -0500

    styling search parameters

--- airbnb/src/components/header/header.jsx
+++ airbnb/src/components/header/header.jsx
@@ -5,10 +5,12 @@ import SearchIcon from "@mui/icons-material/Search";
 function Header() {
   return (
     <div className={styles.searchBar}>
+      <div className={styles.parameterContainer}>
       <SearchParameter title={"Dónde"} subtitle={"Explora destinos"} />
       <SearchParameter title={"Llegada"} subtitle={"Agrega fecha"} />
       <SearchParameter title={"Salida"} subtitle={"Agrega fecha"} />
       <SearchParameter title={"Quién"} subtitle={"¿Cuántos?"} />
+      </div>
       <button className={styles.searchButton}>
         <div>
           <SearchIcon></SearchIcon>
