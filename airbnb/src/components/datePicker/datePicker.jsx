80cdf1bc252e7dc4c58f634fde0630e5bd81cae8
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-24 17:55:40 -0500

    Range in start date and end date implemented

--- airbnb/src/components/datePicker/datePicker.jsx
+++ airbnb/src/components/datePicker/datePicker.jsx
@@ -1,16 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./index.module.css";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers-pro";
+import { DateRangeCalendar } from "@mui/x-date-pickers-pro/DateRangeCalendar";
+// import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import dayjs from "dayjs";
 
+/*
+<LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar
+          key={"first-" + valueFirstCalendar.month()}
+          disablePast
+          disableHighlightToday
+          referenceDate={valueFirstCalendar}
+          value={getValue()}
+          onChange={(newValue) => setFirstCalendarDate(newValue)}
+          views={["day"]}
+        />
+        <DateCalendar
+          key={"second-" + valueSecondCalendar.month()}
+          disablePast
+          disableHighlightToday
+          referenceDate={valueSecondCalendar}
+          onChange={(newValue) => {
+            setValueSecondCalendar(newValue);
+            identifySelected(newValue);
+          }}
+          views={["day"]}
+        />
+      </LocalizationProvider>
+*/
+
 function DatePicker() {
-  const [valueFirstCalendar, setValueFirstCalendar] = useState(null);
-  const [valueSecondCalendar, setValueSecondCalendar] = useState(null);
+  const [valueFirstCalendar, setValueFirstCalendar] = useState(
+    dayjs("2024-01-01")
+  );
+  const [valueSecondCalendar, setValueSecondCalendar] = useState(
+    dayjs("2024-02-01")
+  );
+
+  const [firstCalendarDate, setFirstCalendarDate] = useState(null);
+
   const [monthChange, setMonthChange] = useState(1);
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const elem = ref.current;
+    if (elem == null) return;
+    elem.children[0].children[0].style.display = "none";
+  }, []);
 
   useEffect(() => {
     setValueFirstCalendar(dayjs("2024-" + monthChange + "-01"));
@@ -25,30 +69,37 @@ function DatePicker() {
     setMonthChange(monthChange + 1);
   };
 
+  const identifySelected = (e) => {
+    console.log(e);
+  };
+
+  const getValue = () => {
+    if (!firstCalendarDate) return null;
+    const sy = firstCalendarDate.year();
+    const sm = firstCalendarDate.month();
+
+    const ay = valueFirstCalendar.year();
+    const am = valueFirstCalendar.month();
+
+    if (sy == ay && sm == am) return firstCalendarDate;
+    return null;
+  };
+
   return (
     <div className={styles.calendarContainer}>
-      <NavigateBeforeIcon onClick={() => backMonth()} />
+      <div style={{ cursor: "pointer" }}>
+        <NavigateBeforeIcon onClick={() => backMonth()} />
+      </div>
 
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar
-          disablePast
-          disableHighlightToday
-          referenceDate={dayjs("2024-01-01")}
-          value={valueFirstCalendar}
-          /*onChange={(newValue) => setValue(newValue)}*/
-          views={["day"]}
-        />
-        <DateCalendar
-          disablePast
-          disableHighlightToday
-          referenceDate={dayjs("2024-02-01")}
-          value={valueSecondCalendar}
-          /*onChange={(newValue) => setValue(newValue)}*/
-          views={["day"]}
-        />
-      </LocalizationProvider>
+      <div ref={ref}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateRangeCalendar />
+        </LocalizationProvider>
+      </div>
 
-      <NavigateNextIcon onClick={() => nextMonth()} />
+      <div style={{ cursor: "pointer" }}>
+        <NavigateNextIcon onClick={() => nextMonth()} />
+      </div>
     </div>
   );
 }
