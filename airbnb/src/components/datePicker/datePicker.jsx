a8dcb5cbcdcc7e148c8ad36c6675d220d0278066
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-24 00:25:57 -0500

    calendar month arrows modified

--- airbnb/src/components/datePicker/datePicker.jsx
+++ airbnb/src/components/datePicker/datePicker.jsx
@@ -3,6 +3,8 @@ import styles from "./index.module.css";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import dayjs from 'dayjs';
 
 
@@ -10,10 +12,12 @@ function DatePicker() {
   //const [value, setValue] = useState(dayjs('2024-02-01'))
   return (
     <div className={styles.calendarContainer}>
+      <NavigateBeforeIcon  onClick="#" />
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DateCalendar disablePast disableHighlightToday  views={['day']} />
         <DateCalendar disablePast disableHighlightToday referenceDate={dayjs('2024-02-01')} /*onChange={(newValue) => setValue(newValue)}*/  views={['day']} />
       </LocalizationProvider>
+      <NavigateNextIcon onClick="#" />
     </div>
   );
 }
