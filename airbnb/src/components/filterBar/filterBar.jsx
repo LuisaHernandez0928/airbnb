ee24111bece30746a8e92e15248ab3c53236870a
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-25 16:23:06 -0500

    Created main filter component

--- airbnb/src/components/filterBar/filterBar.jsx
+++ airbnb/src/components/filterBar/filterBar.jsx
@@ -1,9 +1,31 @@
+import { Categories } from "../categories";
 import styles from "./index.module.css";
 
 function FilterBar({ data }) {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.typeHousesContainer}></div>
+      <div className={styles.typeHousesContainer}>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      <Categories/>
+      </div>
       <div className={styles.filterContainer}></div>
     </div>
   );
---
+++ airbnb/src/components/filters/filters.jsx
@@ -0,0 +1,7 @@
+import styles from './index.module.css'
+
+function Filters({data}) {
+  return (<div className={styles.filtersContainer}>Filtros</div>)
+}
+
+export {Filters}
