5800f556abcf523a182fdd1f13059a9f3bcd3bcb
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-25 15:48:41 -0500

    Created 23 example of Airbnb data in JSON format

--- airbnb/src/components/datePicker/datePicker.jsx
+++ airbnb/src/components/datePicker/datePicker.jsx
@@ -3,14 +3,53 @@ import styles from "./index.module.css";
 // import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 // import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 // import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+// import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+// import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers-pro";
 import { DateRangeCalendar } from "@mui/x-date-pickers-pro/DateRangeCalendar";
 import dayjs from "dayjs";
 
 /*
+  const [valueFirstCalendar, setValueFirstCalendar] = useState(
+    dayjs("2024-01-01")
+  );
+  const [valueSecondCalendar, setValueSecondCalendar] = useState(
+    dayjs("2024-02-01")
+  );
+
+  const [firstCalendarDate, setFirstCalendarDate] = useState(null);
+
+  const [monthChange, setMonthChange] = useState(1);
+
+   useEffect(() => {
+    setValueFirstCalendar(dayjs("2024-" + monthChange + "-01"));
+    setValueSecondCalendar(dayjs("2024-" + (monthChange + 1) + "-01"));
+  }, [monthChange]);
+
+  const backMonth = () => {
+    setMonthChange(monthChange - 1);
+  };
+
+  const nextMonth = () => {
+    setMonthChange(monthChange + 1);
+  };
+
+  const identifySelected = (e) => {
+    console.log(e);
+  };
+
+  const getValue = () => {
+    if (!firstCalendarDate) return null;
+    const sy = firstCalendarDate.year();
+    const sm = firstCalendarDate.month();
+
+    const ay = valueFirstCalendar.year();
+    const am = valueFirstCalendar.month();
+
+    if (sy == ay && sm == am) return firstCalendarDate;
+    return null;
+  };
 <div style={{ cursor: "pointer" }}>
         <NavigateBeforeIcon onClick={() => backMonth()} />
       </div>
@@ -42,17 +81,6 @@ import dayjs from "dayjs";
 */
 
 function DatePicker() {
-  const [valueFirstCalendar, setValueFirstCalendar] = useState(
-    dayjs("2024-01-01")
-  );
-  const [valueSecondCalendar, setValueSecondCalendar] = useState(
-    dayjs("2024-02-01")
-  );
-
-  const [firstCalendarDate, setFirstCalendarDate] = useState(null);
-
-  const [monthChange, setMonthChange] = useState(1);
-
   const ref = useRef(null);
 
   useEffect(() => {
@@ -60,35 +88,6 @@ function DatePicker() {
     if (elem == null) return;
     elem.children[0].children[0].style.display = "none";
   }, []);
-
-  useEffect(() => {
-    setValueFirstCalendar(dayjs("2024-" + monthChange + "-01"));
-    setValueSecondCalendar(dayjs("2024-" + (monthChange + 1) + "-01"));
-  }, [monthChange]);
-
-  const backMonth = () => {
-    setMonthChange(monthChange - 1);
-  };
-
-  const nextMonth = () => {
-    setMonthChange(monthChange + 1);
-  };
-
-  const identifySelected = (e) => {
-    console.log(e);
-  };
-
-  const getValue = () => {
-    if (!firstCalendarDate) return null;
-    const sy = firstCalendarDate.year();
-    const sm = firstCalendarDate.month();
-
-    const ay = valueFirstCalendar.year();
-    const am = valueFirstCalendar.month();
-
-    if (sy == ay && sm == am) return firstCalendarDate;
-    return null;
-  };
 
   return (
     <div className={styles.calendarContainer}>
