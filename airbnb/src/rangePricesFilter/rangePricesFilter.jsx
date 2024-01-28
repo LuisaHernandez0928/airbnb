fe49a858e1d207e02dd253f5e848527a1447dc53
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-28 14:23:14 -0500

    PriceSlider component and Slider library installed

--- airbnb/src/rangePricesFilter/rangePricesFilter.jsx
+++ airbnb/src/rangePricesFilter/rangePricesFilter.jsx
@@ -1,12 +1,40 @@
-import { DescriptionFilter } from '../components/descriptionFilter'
-import { TitleFilter } from '../components/titleFilter'
-import styles from './index.module.css'
+import { useState } from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import { DescriptionFilter } from "../components/descriptionFilter";
+import { TitleFilter } from "../components/titleFilter";
+import styles from "./index.module.css";
 
-function RangePrices(){
-  return (<div className={styles.rangePricesLayout}>
-    <TitleFilter text={"Rango precios"} />
-    <DescriptionFilter text={"Precios por noche sin incluir impuestos y tarifas"} />
-  </div>)
+function RangePrices() {
+  const [range, setRange] = useState([30, 70]); // Rango de precios inicial
+
+  const handleSliderChange = (newRange) => {
+    setRange(newRange);
+  };
+
+  return (
+    <div className={styles.rangePricesLayout}>
+      <div className={styles.headerText}>
+        <TitleFilter text={"Rango precios"} />
+        <DescriptionFilter
+          text={"Precios por noche sin incluir impuestos y tarifas"}
+        />
+      </div>
+      <div>
+        <label>Precio Mínimo: ${range[0]}</label>
+        <label style={{ marginLeft: "10px" }}>Precio Máximo: ${range[1]}</label>
+
+        <Slider
+          min={0}
+          max={100}
+          step={1}
+          range
+          value={range}
+          onChange={handleSliderChange}
+        />
+      </div>
+    </div>
+  );
 }
 
-export {RangePrices}
+export { RangePrices };
