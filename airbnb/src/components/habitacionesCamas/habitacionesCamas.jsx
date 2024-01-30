93ae7cad39bc4dee1465bff798062a395aae64e1
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-30 10:49:03 -0500

    created RadioButtonSeparated component

--- airbnb/src/components/habitacionesCamas/habitacionesCamas.jsx
+++ airbnb/src/components/habitacionesCamas/habitacionesCamas.jsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 import { TitleFilter } from "../titleFilter";
 import { DescriptionFilter } from "../descriptionFilter";
 
-function HabitacionesCamas() {
+function HabitacionesCamas({subtitle}) {
   const [buttonSelected, setButtonSelected] = useState("");
 
   const notifyButton = (e) => {
---
+++ airbnb/src/components/radioButtonSeparated/radioButtonSeparated.jsx
@@ -0,0 +1,7 @@
+import styles from './index.module.css'
+
+function RadioButtonSeparated({subtitle}){
+
+}
+
+export {RadioButtonSeparated}
