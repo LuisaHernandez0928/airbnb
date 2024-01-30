d1e2a3050b80ee33a68ee02c21d14fa32bc8c856
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-30 11:10:36 -0500

    styled layout in filterModal

--- airbnb/src/components/habitacionesCamas/habitacionesCamas.jsx
+++ airbnb/src/components/habitacionesCamas/habitacionesCamas.jsx
@@ -1,112 +1,14 @@
-import { useState } from "react";
 import styles from "./index.module.css";
 import { TitleFilter } from "../titleFilter";
-import { DescriptionFilter } from "../descriptionFilter";
+import { RadioButtonSeparated } from "../radioButtonSeparated/radioButtonSeparated";
 
-function HabitacionesCamas({subtitle}) {
-  const [buttonSelected, setButtonSelected] = useState("");
-
-  const notifyButton = (e) => {
-    setButtonSelected(e.target.innerText);
-    console.log(e.target.innerText);
-  };
-
+function HabitacionesCamas() {
   return (
-    <div className={styles.container}>
+    <div className={styles.containerHabitacionesCamas}>
       <TitleFilter text={"Habitacion y camas"} />
-      <DescriptionFilter text={"Habitaciones"} />
-      <div className={styles.radioButtonContainer}>
-        <button
-          className={
-            buttonSelected === "Cualquiera"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          Cualquiera
-        </button>
-        <button
-          className={
-            buttonSelected === "1"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          1
-        </button>
-        <button
-          className={
-            buttonSelected === "2"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          2
-        </button>
-        <button
-          className={
-            buttonSelected === "3"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          3
-        </button>
-        <button
-          className={
-            buttonSelected === "4"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          4
-        </button>
-        <button
-          className={
-            buttonSelected === "5"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          5
-        </button>
-        <button
-          className={
-            buttonSelected === "6"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          6
-        </button>
-        <button
-          className={
-            buttonSelected === "7"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          7
-        </button>
-        <button
-          className={
-            buttonSelected === "8+"
-              ? styles.buttonSelectedA
-              : styles.numericOption
-          }
-          onClick={(e) => notifyButton(e)}
-        >
-          8+
-        </button>
-      </div>
+      <RadioButtonSeparated subtitle={"Habitaciones"}/>
+      <RadioButtonSeparated subtitle={"Camas"}/>
+      <RadioButtonSeparated subtitle={"Baños"}/>
     </div>
   );
 }
