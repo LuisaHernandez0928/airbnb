a81cf7aab134435a76d12e00b4ebdb06887fd2f9
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-31 16:32:14 -0500

    Footer componet created

---
+++ airbnb/src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css'
+
+function Footer() {
+  return (<div className={styles.footerContainer}>
+    <button>Quitar todos</button>
+  </div>)
+}
+
+export {Footer}
