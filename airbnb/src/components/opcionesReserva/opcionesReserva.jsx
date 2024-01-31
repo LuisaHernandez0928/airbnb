20cbb5d792c44426c909591124f54505da7abb3d
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-31 15:30:51 -0500

    Booking options layout

--- airbnb/src/components/opcionesReserva/opcionesReserva.jsx
+++ airbnb/src/components/opcionesReserva/opcionesReserva.jsx
@@ -1,7 +1,29 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
+import {Toggle} from "../toggle"
 
-function OpcionesReserva () {
-  return (<div className={styles.reservasContainer}></div>)
+function OpcionesReserva() {
+  const bookingTitle = [
+    "Reserva inmediata",
+    "Llegada autonoma",
+    "Se permiten mascotas",
+  ];
+  const bookingDescription = [
+    "Anuncios que puedes reservar sin esperar la aprovación del anfitrión",
+    "Fácil acceso a la propiedad una vez que llegues",
+    "¿Traes a un animal de servicio?",
+  ];
+
+  return (
+    <div className={styles.reservasContainer}>
+      {bookingTitle.map((title, index) => (
+        <div>
+          <div>{title} </div>
+           <div>{bookingDescription[index]} </div>
+           <Toggle />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export {OpcionesReserva}
+export { OpcionesReserva };
