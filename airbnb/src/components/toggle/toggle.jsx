4d1b7c0aebca683be720cbac7d6b25f4b3a4d38e
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-31 17:19:38 -0500

    improving UI

--- airbnb/src/components/toggle/toggle.jsx
+++ airbnb/src/components/toggle/toggle.jsx
@@ -7,32 +7,34 @@ function Toggle() {
   console.log("Toggle");
   if (activate) {
     return (
-      <button
-        className={styles.backgroundToggleActivate}
-        onClick={() => setActivate(!activate)}
-      >
-        <div className={styles.frontToggleActive}>
-          <svg
-            viewBox="0 0 12 12"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            role="presentation"
-            focusable="false"
-            style={{
-              display: "block",
-              height: "12px",
-              width: "12px",
-              fill: "currentcolor",
-            }}
-          >
-            <path d="m10.5 1.939 1.061 1.061-7.061 7.061-.53-.531-3-3-.531-.53 1.061-1.061 3 3 5.47-5.469z"></path>
-          </svg>
-        </div>
-      </button>
+      <div style={{ width: "fit-content", height: "54px" }}>
+        <button
+          className={styles.backgroundToggleActivate}
+          onClick={() => setActivate(!activate)}
+        >
+          <div className={styles.frontToggleActive}>
+            <svg
+              viewBox="0 0 12 12"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="presentation"
+              focusable="false"
+              style={{
+                display: "block",
+                height: "12px",
+                width: "12px",
+                fill: "currentcolor",
+              }}
+            >
+              <path d="m10.5 1.939 1.061 1.061-7.061 7.061-.53-.531-3-3-.531-.53 1.061-1.061 3 3 5.47-5.469z"></path>
+            </svg>
+          </div>
+        </button>
+      </div>
     );
   } else {
     return (
-      <div style={{width: "fit-content", height: "54px"}}>
+      <div style={{ width: "fit-content", height: "54px" }}>
         <button
           className={styles.backgroundToggleInactive}
           onClick={() => setActivate(!activate)}
