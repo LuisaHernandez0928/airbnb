124aca6bc9de1d3a4608785140d28b9ec377e11e
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-02-08 17:25:57 -0500

    page structure

--- airbnb/src/pages/addAirbnb/addNewAirbnb.jsx
+++ airbnb/src/pages/addAirbnb/addNewAirbnb.jsx
@@ -44,6 +44,17 @@ function AddAirbnb({options}) {
         cardFilter={true}
       />
       {card}
+      <div>cuantas habitaciones, banos, camas</div>
+      <div>direccion de tu espacio: pais, direccion, ciudad, estado</div>
+      <div> cuantas personas pueden alojarse</div>
+      <div>cuentale a los huespedes todo lo que el espacio puede ofrecer (servicios)</div>
+      <div>agrega fotos</div>
+      <div>ponle un titulo a tu espacio y escribe tu descripcion</div>
+      <div>configuracion del tipo de reservacion</div>
+      <div>establecer precio y disponibilidad</div>
+
+
+
     </div>
   );
 }
